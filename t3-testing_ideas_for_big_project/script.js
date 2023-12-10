500161d52472bed7447622a4c5cf86240cb8e6bd
Karis <90411730+krisCrossApplesauce@users.noreply.github.com>
2023-12-09 18:49:02 -0600

    animations!! :D
    - added the animation classes to be added to the divs so they could be animated
    - made sure the timers didn't interrupt each other
    - also added a background so it didn't show weird white spaces during the animation :)

--- t3-testing_ideas_for_big_project/script.js
+++ t3-testing_ideas_for_big_project/script.js
@@ -1,34 +1,53 @@
 const boing = new Audio('./assets/jump.mp3');
+boing.playbackRate = 1.7;
+boing.volume = 0.02;
+var timer = null;
 
-function scrollThroughTrees(event) {
-	if (event.deltaY > 0) {
+
+function animateThroughTrees(event) {
+	if (event.deltaY > 0 && timer === null) {
+
 		$("#tree-0").attr("id", "1");
 		$("#tree-1").attr("id", "2");
 		$("#tree-2").attr("id", "3");
 		$("#tree-3").attr("id", "0");
 
-		$("#0").attr("class", "tree z-0 m-auto");
-		$("#0").hide();
-		$("#1").attr("class", "tree z-1 size-1 m-auto");
-		$("#1").show();
-		$("#2").attr("class", "tree z-2 size-2 m-auto");
-		$("#3").attr("class", "tree z-3 m-auto");
+		$("#1").addClass("moveTo1");
+		$("#2").addClass("moveTo2");
+		$("#3").addClass("moveTo3");
+		$("#0").addClass("moveTo0");
 
-		$("#0").attr("id", "tree-0");
-		$("#1").attr("id", "tree-1");
-		$("#2").attr("id", "tree-2");
-		$("#3").attr("id", "tree-3");
+		timer = setTimeout(scrollThroughTrees, 4000);
 	}
-	else {
+	else if (event.deltaY <= 0) {
 		console.log("scrolled down");
 	}
-
-	boing.load();
-	boing.playbackRate = 1.7;
-	boing.play();
-	console.log("function has been called");
+	else {
+		console.log("waiting for timer before going again");
+	}
 }
 
+function scrollThroughTrees() {
+	$("#0").attr("class", "tree z-0 m-auto hide");
+/*	$("#0").hide(); */
+	$("#1").attr("class", "tree z-1 size-1 m-auto");
+/*	$("#1").show(); */
+	$("#2").attr("class", "tree z-2 size-2 m-auto");
+	$("#3").attr("class", "tree z-3 m-auto");
+
+	$("#0").attr("id", "tree-0");
+	$("#1").attr("id", "tree-1");
+	$("#2").attr("id", "tree-2");
+	$("#3").attr("id", "tree-3");
+
+	boing.play();
+	clearTimeout(timer);
+	timer = null;
+	console.log("scroll function has been called");
+}
+
+/**
 $(document).ready(function() {
 	$("#tree-0").hide();
 });
+*/
